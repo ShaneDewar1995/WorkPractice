75317d6b3b4d2106798a64482081f7e4a9fb748e
Shane <>
2020-05-06 15:18:17 +0200

    changed value to 6

--- HelloWorld/Program.cs
+++ HelloWorld/Program.cs
@@ -6,7 +6,7 @@ namespace HelloWorld
     class Program
     {
 
-        public static int firstNumber = 3;
+        public static int firstNumber = 6;
         public static int secondNumber = 3;
 
 
